fa55c5622a65dde7114855c08ecc9589c0a8c300
Murka1456 <mal-bod2@hotmail.com>
2025-03-16 15:58:02 +0100

    addcolor

--- script.js
+++ script.js
@@ -1,33 +1,23 @@
-
 let newTextContainer = document.querySelector('#newTextContainer');
-//KNAPP JEG HAR LAGET SELV
-const button = document.querySelector('#button');
-function Myfunction() {
-  console.log('beskjed fra function');
-}
-
-button.addEventListener('click', Myfunction);
-
-// letaddTex
-
-//KNAPP FOR FARGER
-let addColor = document.querySelector('#addColor');
-
-addColor.addEventListener('click', function () {
-  console.log('dette er knapp for farger');
-});
+let addText = document.querySelector("#addText");
 
 //KNAPP FOR TEKST
 let addText = document.querySelector('#addText');
 
 addText.addEventListener('click', function () {
   let newText = document.createElement('h1');
-  newText.textContent = 'Dette er ny text';
+  newText.textContent = 'Dette er ny tekst';
   newTextContainer.appendChild(newText);
 
-  console.log('dette er knapp for TEKST');
 });
-// /knapp for farge
+let styleDiv = document.querySelector("#slyleContainer");
+//knapp for farge
+let addColor = document.querySelector("#addColor");
+let addColor.addEventListener("click",function(){
+  styleDiv.classList.add("red");
+});
+
+})
 let styleContainer = document.querySelector('#styleContainer');
 let Pushknapp = document.querySelector('#Pushknapp');
 Pushknapp.addEventListener('click', function () {
