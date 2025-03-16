05ca0e3aae5bcc89d0c4aa3ff712c05cf767aa92
Murka1456 <mal-bod2@hotmail.com>
2025-03-16 11:47:58 +0100

    ok

--- script.js
+++ script.js
@@ -1,56 +1,4 @@
-/* OPPGAVE:
-  Lag en ny knapp i HTML og få knappen til å console.log en beskjed du selv velger
 
-  Lag en steg for steg liste med kommentarer for hva du må gjøre for å få det til 
-
-  Eks. 1. lag en knapp
-       2. 
-
-
- */
-
-//FÅ TEKST UT PÅ SIDEN
-
-/**
- 
-1. Hent inn div ved å bruke ID
-2. Lage nytt element
-3. Legge til tekst på elementet
-4. Legge til det nye elementet i div
-5. Legge det inn i en addEventListener, så det kun skjer når vi trykker på knappen
-
- */
-
-//ENDRE STYLING I JS
-/*
-classList.add = legger til en class
-classlist.remove = tar bort en class
-classlist.toggle = skifter mellom å legge til og ta bort klassenavn
-*/
-
-/*
-1. hente inn id til div
-2. hent inn id til fargeknapp
-3. lag en eventlistener til knappen
-4. lagge til classList med ny bakgrunnsfarge i eventlistener
-
-*/
-
-//OPPGAVE
-
-/*
-1. Lag en ny div med en id
-2. Legg til en knapp eller bruk en av knappene som er der allerede og hent inn id til JS
-3. Lag en eventlistener til knappen
-4. Legg til noe tekst til div
-5. lag en class med style til knappen som du legger på med classList når du trykker på knappen
-
-Bonus!
-
-Lag en animasjon som blir aktivert når du trykker på knappen
-
-
-*/
 let newTextContainer = document.querySelector('#newTextContainer');
 
 //KNAPP JEG HAR LAGET SELV
@@ -86,3 +34,19 @@ let Pushknapp = document.querySelector('#Pushknapp');
 Pushknapp.addEventListener('click', function () {
   styleContainer.classList.add('yellow');
 });
+let newTextContainer = document.querySelector('#newTextContainer');
+
+//Dette er knapp for tekst
+let addText = document.querySelector('#addText');
+
+//Dette er eventListener for tekst
+addText.addEventListener('click', function () {
+  let newText = document.createElement('h1'); //her lager vi tekstelement
+
+  newText.textContent = 'Dette er ny tekst'; //her legger vi til tekst på elementet
+
+  newTextContainer.appendChild(newText); //Her legger vi variabel med tekst inn i div i HTML
+});
+
+//
+//
